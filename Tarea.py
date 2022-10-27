1d5ffb2fab4fd0e4dbafca5ec9308672ddd79b56
Tomasmogu <115584945+Tomasmogu@users.noreply.github.com>
2022-10-27 17:51:29 -0500

    Create Tarea.py

---
+++ Tarea.py
@@ -0,0 +1,8 @@
+import math
+
+g = float(input("Introduce un numero en grados que desee pasar a radianes: "))
+
+rad1 = math.radians(g)
+
+print("Con Math\n")
+print(str(g) + " grados son " + str(rad1) + " radianes\n")
